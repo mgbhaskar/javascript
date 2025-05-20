d4d80b5af2e940ba86b2c6408f3394af199d510c
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-05-20 08:11:26 +0530

    javascript update

---
+++ index.js
@@ -0,0 +1,7 @@
+let integer = 42;      // Integer
+let float = 3.14;      // Floating-point number
+let scientific = 1e3;  // Scientific notation (1000)
+
+console.log(integer);  // number
+console.log(float);    // number
+console.log(typeof scientific);      // 1000
