0a79bd14d38c89507d4b187df07c43fb4771c08a
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-05-21 21:28:23 +0530

    non primitive

---
+++ 4-script.js
@@ -0,0 +1,64 @@
+ //non primitive
+
+ // 1)Object example
+
+        //   let person = {
+        //      name: "bhaskar",
+        //      age: 25,
+        //      greet: function() {
+        //      console.log("Hello, " + this.name);
+        //         }
+        //      };
+
+        //     person.greet(); 
+
+
+// 2) Array
+
+            //   let colors = ["red", "green", "blue"];
+
+            //   console.log(colors[0]);
+            //   console.log(colors.length);
+
+  
+              
+//3)Function
+
+
+            //  function add(a, b) {
+            //   return a + b;
+            //     }
+
+            //  console.log(add(5, 3)); // Output: 8
+
+
+//  5)Date
+
+        //   let today = new Date();
+
+        //   console.log(today); // Output: current date and time
+
+
+ //5) Regular Expression
+
+                let pattern = /[A-Z]/g;
+
+                console.log("Hello".match(pattern)); // Output: ['H']
+                
+
+
+// 6)Map
+
+                    // let map = new Map();
+                    // map.set("key1", "value1");
+                    // map.set("key2", "value2");
+                    
+                    // console.log(map.get("key1")); // Output: value1
+                    
+
+//  7)Sets
+
+               let uniqueNumbers = new Set([1, 2, 3, 2, 1]);
+
+               console.log(uniqueNumbers); // Output: Set(3) {1, 2, 3}
+               
