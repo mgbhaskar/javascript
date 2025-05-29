934921a3003f93d3f79de18fe6e754be6163acf9
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-05-29 21:50:38 +0530

    function

---
+++ 16-script.js
@@ -0,0 +1,27 @@
+// block1
+{
+    let movieID = 501;
+
+
+    
+    console.log(movieID);
+  
+    let movieTitle = "RRR";
+    console.log(movieTitle);
+  }
+  
+  // block2
+  {
+    let movieNames = ["RRR", "baahubali", "Pushpa"]; 
+
+
+            console.log(movieNames);
+  
+    let actorNames = ["ntr", "prabhas", "alluarjun"];
+
+            console.log(actorNames);
+  
+      let releaseYears = [2022, 2015, 2021];
+                   console.log(releaseYears);
+  }
+  
