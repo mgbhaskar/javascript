d8112356012ea09fd2eccb40a82892def128c209
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-05-30 22:23:57 +0530

    coparisons

---
+++ 17-script.js
@@ -0,0 +1,48 @@
+// console.log(5 == '5');     
+// console.log(0 == false);   // true → 0 is considered falsy
+
+
+// console.log(5 != '5');     // false → values are equal
+// console.log(3 != 4);       // true → different values
+
+
+// console.log(5 === '5');    // false → number vs string
+// console.log(10 === 10);    // true → both value and type match
+
+
+
+// console.log(5 !== '5');    // true → different types
+// console.log(10 !== 10);    // false → same value and type
+
+
+
+// console.log(7 > 5);        // true → 7 is greater than 5
+// console.log(3 > 10);       // false → 3 is not greater than 10
+
+
+
+
+
+
+
+// console.log(2 < 8);        // true → 2 is less than 8
+// console.log(5 < 3);        // false → 5 is not less than 3
+
+
+
+console.log(10 >= 10);     // true → equal
+console.log(15 >= 12);     // true → greater
+console.log(9 >= 10);      // false → less
+
+console.log(8 <= 8);       // true → equal
+console.log(6 <= 10);      // true → less
+console.log(12 <= 9);      // false → greater
+
+
+
+
+
+
+
+
+
