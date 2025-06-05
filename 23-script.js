92680eac0b793ea938fb7726f6cf5794fc959034
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-06-05 21:28:42 +0530

    callback hell

---
+++ 23-script.js
@@ -0,0 +1,54 @@
+//  callback hell
+
+
+
+// function stepone(callbackFn){
+//     console.log("step one");
+//     callbackFn();
+// }
+// function steptwo(callbackFn){
+//     console.log("step two");
+//     callbackFn();
+// }
+// function stepthree(){
+//     console.log("step three");
+// }
+
+// stepone(()=>{
+//     steptwo(()=>{
+//         stepthree();
+//     });
+// })
+
+
+
+
+function announceDevara(callback) {
+    setTimeout(() => {
+        console.log("Devara movie announced");
+        callback();
+    }, 2000);
+}
+
+function shootDevara(callback) {
+    setTimeout(() => {
+        console.log("Devara movie shooting started");
+        callback();
+    }, 3000);
+}
+
+function releaseDevara(callback) {
+    setTimeout(() => {
+        console.log("Devara movie released");
+        callback();
+    }, 3000);
+}
+
+// Callback Hell
+announceDevara(() => {
+    shootDevara(() => {
+        releaseDevara(() => {
+            console.log("Devara movie journey completed");
+        });
+    });
+});
